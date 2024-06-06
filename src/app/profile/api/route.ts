a67ffee4b453b0669a941d359c6ebf44a467ba38
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-06 12:25:58 +0100

    Commit

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,7 +1,13 @@
+import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
   console.log(requestHeaders.get("Authorization"));
-  return new Response("Profile API data ...");
+  console.log(headerList.get("Authorization"));
+  console.log(requestHeaders.get("Authorization"));
+  return new Response("<h1>Profile API data ...</h1>", {headers: {
+    "Content-Type": "text/html"
+  }});
 };
